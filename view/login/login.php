638dbd2e00f066a7cd9560ab5649ee9bdeb41205
Renan Pollo Benelli <Renansk18@outlook.com>
2023-06-17 23:40:52 -0300

    commit

--- view/login/login.php
+++ view/login/login.php
@@ -7,12 +7,71 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="../../view/css/login.css">
         <title>Login</title>
     </head>
     <body>
+    <section class="container">
+        <div class="login">
+            <div class="wrapper">
+                <img src="./assets/img/logo.png" alt="logo-riot" class="login-logo">
+
+                <h1 class="login-title">Fazer Login</h1>
+
+                <div class="textfield">
+                    <label for="User-name" class="login-label">
+                        <span>Nome de Usuario</span>
+                        <input type="text" name="user-name" id="user-name" class="input">
+                    </label>
+                </div>
+
+                <div class="textfield">
+                    <label for="password" class="login-label">
+                        <span>Senha</span>
+                        <input type="password" name="password" id="password" class="input">
+                    </label>
+                </div>
+
+                <div class="login-icons">
+                    <button type="button" class="icon-button">
+                        <img src="./assets/img/facebook-icon.png" alt="facebook-icon">
+                    </button>
+
+                    <button type="button" class="icon-button">
+                        <img src="./assets/img/google-icon.png" alt="google-icon">
+                    </button>
+
+                    <button type="button" class="icon-button">
+                        <img src="./assets/img/apple-icon.png" alt="apple-icon">
+                    </button>
+                </div>
+
+                <div class="textfield-checkbox">
+                    <label for="checkbox" class="login-label-checkbox">
+                        <input type="checkbox" name="checkbox" class="input-checkbox">Manter Login
+                    </label>
+                </div>
+            </div>
             
-        
-        <script src="" async defer></script>
-    </body>
+            <div class="wrapper">
+                <div>
+                    <button type="button" class="login-btn" disabled>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path
+                                d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z" />
+                        </svg>
+                    </button>
+                </div>
+
+                <a href="#" class="login-link">não consegue iniciar sessão?</a>
+                <a href="#" class="login-link">criar conta</a>
+            </div>
+        </div>
+
+        <div class="wallpaper">
+
+        </div>
+    </section>
+    <script src="../../view/js/login.js" async defer></script>
+</body>
 </html>
